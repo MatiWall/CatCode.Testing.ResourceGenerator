10a934648ac8d8819627be1b02b1afd1f5bd944f
Matias Wallenius <MatiWall@live.dk>
2024-09-24 21:15:10 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+import logging
+logger = logging.getLogger(__name__)
+import subprocess
+from pathlib import Path
+import requests
+import git
+import os
+import yaml
+
+from settings import BASE_DIR
+
+
+# Clone the repository
+def clone_repo(repo_url, clone_dir: Path):
+    if not not clone_dir.exists():
+        clone_dir.mkdir(parents=True, exist_ok=True)
+    git.Repo.clone_from(repo_url, clone_dir)
+
+def update_repo(clone_dir):
+    subprocess.run(['git', 'pull'], cwd=clone_dir)
+
+# Read files from the repository
+def read_files(directory):
+    objects = []
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = directory / file
+            with file_path.open('r') as f:
+                obj = yaml.safe_load(f)
+
+            objects.append(obj)
+    return objects
+
+
+def insert_files_to_etcd(files):
+
+    for file in files: # all logic is left for the CoreAI
+        resp = requests.post('etcd_host: "http://localhost:8000"', json=file)
+        logger.info(str(resp.json()))
+
+
+
+# Main function
+def main():
+
+    CLONE_REPO = BASE_DIR/ f'clones/'
+
+    try:
+        update_repo(CLONE_REPO)
+    except:
+        clone_repo(
+            'https://github.com/MatiWall/CatCode.Catalog.Models.git',
+            CLONE_REPO
+        )
+    files = read_files(CLONE_REPO / 'kinds')
+
+    insert_files_to_etcd(files)
+
+if __name__ == "__main__":
+    main()
